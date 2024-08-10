46adff8079fe27a00b54079936b4ad739421ccc5
miladevweb <miladevweb@outlook.com>
2024-08-10 12:33:30 -0500

    add methods to fetch several images

--- scripts/defer.js
+++ scripts/defer.js
@@ -1,44 +1,55 @@
-function createImage(blobUrl) {
-  const pictureContainer = document.querySelector('#picture-container')
+const urls = [
+  'https://scontent.flim30-1.fna.fbcdn.net/v/t1.6435-9/92097597_2700743383480247_2977929687944134656_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=13d280&_nc_ohc=1iz1-xzZxtAQ7kNvgHYRIKl&_nc_ht=scontent.flim30-1.fna&oh=00_AYAh1vOtIRQQ1b_OxEALRWcj33BWcjQQ1s-9aFnPIRZsSA&oe=66DE42D3',
+  'https://imgix.bustle.com/uploads/getty/2024/6/14/97963092/incheon-south-korea-march.jpg?w=414&h=259&fit=crop&crop=faces&q=50&dpr=2',
+]
+
+const pictureContainers = document.querySelectorAll('[data-picture-container]')
+
+function loaded(container, url) {
+  container.classList.add('loaded')
+  URL.revokeObjectURL(url)
+}
+
+function createImage(url, i) {
   const fragment = document.createDocumentFragment()
   const picture = document.createElement('picture')
 
+  const pictureContainer = pictureContainers[i]
+
   picture.innerHTML = `
         <source
+          srcset="${url}"
           type="image/webp"
-          srcset="${blobUrl}"
         />
         <img
           alt="#"
+          src="${url}"
           loading="lazy"
-          src="${blobUrl}"
         />
-`
-
+    `
   const img = picture.querySelector('img')
 
-  function loaded() {
-    pictureContainer.classList.add('loaded')
-    URL.revokeObjectURL(blobUrl)
-  }
-
-  if (img.complete) loaded()
-  else img.addEventListener('load', loaded)
+  if (img.complete) loaded(pictureContainer, url)
+  else img.addEventListener('load', () => loaded(pictureContainer, url))
 
   fragment.append(picture)
   pictureContainer.append(fragment)
 }
 
-const url =
-  'https://scontent.flim30-1.fna.fbcdn.net/v/t1.6435-9/92097597_2700743383480247_2977929687944134656_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=13d280&_nc_ohc=1iz1-xzZxtAQ7kNvgHYRIKl&_nc_ht=scontent.flim30-1.fna&oh=00_AYAh1vOtIRQQ1b_OxEALRWcj33BWcjQQ1s-9aFnPIRZsSA&oe=66DE42D3'
-
-fetch(url)
-  .then((res) => res.blob())
-  .then((blob) => {
-    const blobUrl = URL.createObjectURL(blob)
-    createImage(blobUrl)
+// Fetching image
+function fetchImage() {
+  urls.forEach((url, i) => {
+    fetch(url)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const blobUrl = URL.createObjectURL(blob)
+        createImage(blobUrl, i)
+      })
   })
+}
+fetchImage()
 
+// Theme Switcher
 document.querySelector('button').addEventListener('click', () => {
   if (document.documentElement.classList.contains('dark')) {
     document.documentElement.classList.remove('dark')
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 const theme = localStorage.getItem('theme')
 const doc = document.documentElement
 
+// Set up theme on load
 if (theme === 'dark') {
   doc.classList.remove('light')
   doc.classList.add('dark')
 } else if (theme === 'light') {
   doc.classList.remove('dark')
   doc.classList.add('light')
-} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+} else if (matchMedia('(prefers-color-scheme: dark)').matches) {
   doc.classList.add('dark')
   localStorage.setItem('theme', 'dark')
 } else {
